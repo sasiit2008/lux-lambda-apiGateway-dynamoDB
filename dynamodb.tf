8d755dfb44848d16c0fda30b30d27872b6a2066c
info <info@rivitech.co>
2021-08-26 10:16:30 +0530

    lambda added

--- dynamodb.tf
+++ dynamodb.tf
@@ -63,69 +63,27 @@ resource "aws_dynamodb_table" "menu" {
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "ItemMenu"
+  range_key      = "LangCode"
   attribute {
     name = "ItemMenu"
     type = "S"
   }
-  # attribute {
-  #   name = "LangCode"
-  #   type = "S"
-  # }
+  attribute {
+    name = "LangCode"
+    type = "S"
+  }
 
-  # attribute {
-  #   name = "SubModule"
-  #   type = "S"
-  # }
+  attribute {
+    name = "SubModule"
+    type = "S"
+  }
 
-  # attribute {
-  #   name = "Message"
-  #   type = "S"
-  # }
+  attribute {
+    name = "Message"
+    type = "S"
+  }
 
   tags = merge({ Name = "${var.project}-chatbot-menu-${var.environment}-dynamodb" }, tomap(var.additional_tags))
 }
 
 
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
-
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  attribute {
-    name = "GameTitle"
-    type = "S"
-  }
-
-  attribute {
-    name = "TopScore"
-    type = "N"
-  }
-
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
-  global_secondary_index {
-    name               = "GameTitleIndex"
-    hash_key           = "GameTitle"
-    range_key          = "TopScore"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
-  }
-
-  tags = {
-    Name        = "dynamodb-table-1"
-    Environment = "production"
-  }
-}
