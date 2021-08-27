e832e6cf1e63217cbfd1d30de23b2ef6da074810
info <info@rivitech.co>
2021-08-27 17:55:01 +0530

    api modified

--- api.tf
+++ api.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "getTranslation" {
 resource "aws_api_gateway_resource" "getTranslation" {
   rest_api_id = aws_api_gateway_rest_api.getTranslation.id
   parent_id   = aws_api_gateway_rest_api.getTranslation.root_resource_id
-  path_part   = "/"
+  path_part   = "/gettranslatetdtext"
 }
 
 # resource "aws_api_gateway_method" "get" {
