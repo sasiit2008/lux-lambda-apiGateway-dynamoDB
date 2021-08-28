9638ce7c69b5a046560f6fd0a9b3a01338035b46
info <info@rivitech.co>
2021-08-28 18:56:47 +0530

    lex modified

--- api.tf
+++ api.tf
@@ -59,7 +59,7 @@ resource "aws_api_gateway_integration" "getTranslation" {
 }
 
 resource "aws_api_gateway_integration_response" "getTranslation" {
-  http_method = aws_api_gateway_method.getTranslation.http_method
+  http_method = "POST"
   rest_api_id = aws_api_gateway_rest_api.getTranslation.id
   resource_id = aws_api_gateway_resource.getTranslation.id
   response_parameters = {
