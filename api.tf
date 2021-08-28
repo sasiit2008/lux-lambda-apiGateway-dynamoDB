79a2adeb177eb3dea76e6de3295c48fcc8b69b4c
info <info@rivitech.co>
2021-08-28 19:13:24 +0530

    apigateway cloudwatch enabled

--- api.tf
+++ api.tf
@@ -72,3 +72,94 @@ resource "aws_api_gateway_integration_response" "getTranslation" {
 }
 
 
+resource "aws_api_gateway_deployment" "getTranslation" {
+  depends_on  = [aws_api_gateway_integration.getTranslation]
+  rest_api_id = aws_api_gateway_rest_api.getTranslation.id
+  stage_name  = "dev"
+}
+
+resource "aws_api_gateway_stage" "getTranslation" {
+  stage_name    = "NProd"
+  rest_api_id   = aws_api_gateway_rest_api.getTranslation.id
+  deployment_id = aws_api_gateway_deployment.getTranslation.id
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.cloudlogs.arn
+    format          = <<EOF
+{ "requestId":"$context.requestId", "ip": "$context.identity.sourceIp", "caller":"$context.identity.caller", "user":"$context.identity.user","requestTime":"$context.requestTime", "httpMethod":"$context.httpMethod","resourcePath":"$context.resourcePath", "status":"$context.status","protocol":"$context.protocol", "responseLength":"$context.responseLength" }
+EOF
+  }
+}
+
+resource "aws_api_gateway_method_settings" "getTranslation" {
+  rest_api_id = aws_api_gateway_rest_api.getTranslation.id
+  stage_name  = aws_api_gateway_stage.getTranslation.stage_name
+  method_path = "${trimprefix(aws_api_gateway_resource.source.path, "/")}/${aws_api_gateway_method.post.http_method}"
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
+resource "aws_api_gateway_method_settings" "getTranslation" {
+  rest_api_id = aws_api_gateway_rest_api.getTranslation.id
+  stage_name  = aws_api_gateway_stage.getTranslation.stage_name
+  method_path = "${trimprefix(aws_api_gateway_resource.getTranslation.path, "/")}/${aws_api_gateway_method.getTranslation.http_method}"
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
+
+resource "aws_api_gateway_account" "act" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch.arn
+}
+
+resource "aws_cloudwatch_log_group" "cloudlogs" {
+  name = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.getTranslation.id}/prod"
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  name               = "${var.project}-${var.environment}-getTranslation-apigateway-cloudwatch"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cloudwatch" {
+  name = "${var.project}-${var.environment}-getTranslation-apigateway-cloudwatch-policy"
+  role = aws_iam_role.cloudwatch.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
