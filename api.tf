9bf11b8a63ec2dca5623b6ae0ccb3f6f5d03ff86
info <info@rivitech.co>
2021-08-28 13:40:01 +0530

    api aws_api_gateway_integrtion added

--- api.tf
+++ api.tf
@@ -31,16 +31,31 @@ resource "aws_api_gateway_method" "getTranslation" {
 }
 
 resource "aws_api_gateway_method_response" "getTranslation" {
-    http_method         = "POST"
-    rest_api_id = aws_api_gateway_rest_api.getTranslation.id
-    resource_id = aws_api_gateway_resource.getTranslation.id
-    response_models     = {
-        "application/json" = "Empty"
-    }
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = false
-    }
-    status_code         = "200"
+  http_method = "POST"
+  rest_api_id = aws_api_gateway_rest_api.getTranslation.id
+  resource_id = aws_api_gateway_resource.getTranslation.id
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = false
+  }
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration" "getTranslation" {
+  connection_type         = "INTERNET"
+  content_handling        = "CONVERT_TO_TEXT"
+  http_method             = "POST"
+  integration_http_method = "POST"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  request_parameters      = {}
+  request_templates       = {}
+  rest_api_id             = aws_api_gateway_rest_api.getTranslation.id
+  resource_id             = aws_api_gateway_resource.getTranslation.id
+  timeout_milliseconds    = 29000
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${region}:lambda:path/2015-03-31/functions/${aws_lambda_function.translate.arn}/invocations"
 }
 
 
