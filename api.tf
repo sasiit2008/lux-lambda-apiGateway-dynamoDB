95b9e85f714658e9595756ac32be215727bacf51
info <info@rivitech.co>
2021-08-28 13:46:45 +0530

    api aws_api_gateway_integrtion_respose added

--- api.tf
+++ api.tf
@@ -59,15 +59,15 @@ resource "aws_api_gateway_integration" "getTranslation" {
 }
 
 resource "aws_api_gateway_integration_response" "getTranslation" {
-    http_method         = http_method = aws_api_gateway_method.getTranslation.http_method
-    rest_api_id = aws_api_gateway_rest_api.apiLambda.id
-    resource_id = aws_api_gateway_resource.apiLambda.id
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    }
-    response_templates  = {
-        "application/json" = ""
-    }
-    status_code         = "200"
+  http_method = aws_api_gateway_method.getTranslation.http_method
+  rest_api_id = aws_api_gateway_rest_api.apiLambda.id
+  resource_id = aws_api_gateway_resource.apiLambda.id
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+  response_templates = {
+    "application/json" = ""
+  }
+  status_code = "200"
 }
 
