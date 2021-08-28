953efd820f8bc5eb6c5f30555f1bad29525fa986
info <info@rivitech.co>
2021-08-28 13:25:53 +0530

    api added

--- api.tf
+++ api.tf
@@ -20,7 +20,7 @@ resource "aws_api_gateway_rest_api" "getTranslation" {
 # }
 
 resource "aws_api_gateway_resource" "getTranslation" {
-    path_part   = "/"
+    path_part   = "/gettranslatetdtext"
     rest_api_id = aws_api_gateway_rest_api.getTranslation.id
     parent_id   = aws_api_gateway_rest_api.getTranslation.root_resource_id
 }
