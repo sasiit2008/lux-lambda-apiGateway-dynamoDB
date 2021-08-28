4bfc9f94a053ef8f6b27d3c9a7b2bbc430215dbd
info <info@rivitech.co>
2021-08-28 13:28:52 +0530

    api added

--- api.tf
+++ api.tf
@@ -1,45 +1,40 @@
 # # api.tf
 
 resource "aws_api_gateway_rest_api" "getTranslation" {
-    api_key_source               = "HEADER"
-    description                  = "API to trigger lambda for translation"
-    disable_execute_api_endpoint = false
-    minimum_compression_size     = -1
-    name                         = "getTranslation"
-    tags = merge({ Name = "${var.project}-${var.environment}-getTranslation-apigateway" }, tomap(var.additional_tags))
-    endpoint_configuration {
-        types            = [
-            "REGIONAL",
-        ]
-    }
+  api_key_source               = "HEADER"
+  description                  = "API to trigger lambda for translation"
+  disable_execute_api_endpoint = false
+  minimum_compression_size     = -1
+  name                         = "getTranslation"
+  tags                         = merge({ Name = "${var.project}-${var.environment}-getTranslation-apigateway" }, tomap(var.additional_tags))
+  endpoint_configuration {
+    types = [
+      "REGIONAL",
+    ]
+  }
 }
-
-# resource "aws_api_gateway_rest_api" "getTranslation" {
-#   name = "getTranslation${var.environment}"
-#   tags = merge({ Name = "${var.project}-${var.environment}-getTranslation-apigateway" }, tomap(var.additional_tags))
-# }
 
 resource "aws_api_gateway_resource" "getTranslation" {
-    path_part   = "/gettranslatetdtext"
-    rest_api_id = aws_api_gateway_rest_api.getTranslation.id
-    parent_id   = aws_api_gateway_rest_api.getTranslation.root_resource_id
+  path_part   = "/gettranslatetdtext"
+  rest_api_id = aws_api_gateway_rest_api.getTranslation.id
+  parent_id   = aws_api_gateway_rest_api.getTranslation.root_resource_id
+}
+
+resource "aws_api_gateway_method" "getTranslation" {
+  api_key_required   = false
+  authorization      = "NONE"
+  http_method        = "POST"
+  id                 = "agm-pdzr2m3hvk-ql7hei-POST"
+  request_models     = {}
+  request_parameters = {}
+  rest_api_id        = aws_api_gateway_rest_api.getTranslation.id
+  resource_id        = aws_api_gateway_resource.getTranslation.id
 }
 
 
-# resource "aws_api_gateway_resource" "getTranslationRoot" {
-#   rest_api_id = aws_api_gateway_rest_api.getTranslation.id
-#   path_part   = "/"
-# }
-
-# resource "aws_api_gateway_resource" "getTranslation" {
-#   rest_api_id = aws_api_gateway_rest_api.getTranslation.id
-#   parent_id   = aws_api_gateway_rest_api.getTranslation.root_resource_id
-#   path_part   = "/gettranslatetdtext"
-# }
 
 # resource "aws_api_gateway_method" "get" {
-#   rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
-#   resource_id   = aws_api_gateway_resource.apiLambda.id
+
 #   http_method   = "GET"
 #   authorization = "NONE"
 # }
