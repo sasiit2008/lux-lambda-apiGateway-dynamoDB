cfa16f4df5733ba0b75d571d207cfcafc94c9a20
info <info@rivitech.co>
2021-08-29 18:17:35 +0530

    lex default added

--- lex.tf
+++ lex.tf
@@ -1,3 +1,38 @@
+resource "aws_lex_bot" "order_flowers_bot" {
+  abort_statement {
+    message {
+      content      = "Sorry, I am not able to assist at this time"
+      content_type = "PlainText"
+    }
+  }
+
+  child_directed = false
+
+  clarification_prompt {
+    max_attempts = 2
+
+    message {
+      content      = "I didn't understand you, what would you like to do?"
+      content_type = "PlainText"
+    }
+  }
+
+  create_version              = false
+  description                 = "Bot to order flowers on the behalf of a user"
+  idle_session_ttl_in_seconds = 600
+
+  intent {
+    intent_name    = "OrderFlowers"
+    intent_version = "1"
+  }
+
+  locale           = "en-US"
+  name             = "OrderFlowers"
+  process_behavior = "BUILD"
+  voice_id         = "Salli"
+}
+
+
 # resource "aws_lex_bot" "lex" {
 #     child_directed                  = false
 #     create_version                  = true
