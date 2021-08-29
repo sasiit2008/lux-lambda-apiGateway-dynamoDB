bdaf194a29cb58cfa578252bb8dd9b9341d44d81
info <info@rivitech.co>
2021-08-29 18:24:05 +0530

    lex default added

--- lex.tf
+++ lex.tf
@@ -1,3 +1,10 @@
+resource "aws_lex_bot_alias" "order_flowers_prod" {
+  bot_name    = "OrderFlowers"
+  bot_version = "1"
+  description = "Production Version of the OrderFlowers Bot."
+  name        = "OrderFlowersProd"
+}
+
 resource "aws_lex_bot" "order_flowers_bot" {
   abort_statement {
     message {
