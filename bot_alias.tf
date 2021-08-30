f71a6eeb71bf456b5a2147ff0a60088c776c42ca
info <info@rivitech.co>
2021-08-30 20:26:13 +0530

    lex name added

--- bot_alias.tf
+++ bot_alias.tf
@@ -2,5 +2,7 @@ resource "aws_lex_bot_alias" "LATIS_greetingUser_dev" {
   bot_name    = aws_lex_bot.vwt_corp_chatbot_latis_iat_one.name
   bot_version = aws_lex_bot.vwt_corp_chatbot_latis_iat_one.version
   description = "Development version of bot to make chatbot"
-  name        = "LATIS_greetingUser_dev"
+  # name        = "LATIS_greetingUser_dev"
+  name = var.environment == "dev" ? "vwt_corp_chatbot_latis" : "vwt_corp_chatbot_latis${var.environment}"
+
 }
