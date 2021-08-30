fc42b1b8f19865d47354ccd490a65f391a561e45
info <info@rivitech.co>
2021-08-30 11:48:26 +0530

    lex added

---
+++ bot-data.tf
@@ -0,0 +1,14 @@
+data "aws_lex_bot" "vwt_corp_chatbot_latis_iat_one" {
+  name    = aws_lex_bot.vwt_corp_chatbot_latis_iat_one.name
+  version = aws_lex_bot.vwt_corp_chatbot_latis_iat_one.version
+}
+
+data "aws_lex_bot_alias" "LATIS_greetingUser_dev" {
+  bot_name = aws_lex_bot_alias.LATIS_greetingUser_dev.bot_name
+  name     = aws_lex_bot_alias.LATIS_greetingUser_dev.name
+}
+
+data "aws_lex_intent" "LATIS_greetingUser" {
+  name    = aws_lex_intent.LATIS_greetingUser.name
+  version = aws_lex_intent.LATIS_greetingUser.version
+}
---
+++ bot.tf
@@ -0,0 +1,40 @@
+resource "aws_lex_bot" "vwt_corp_chatbot_latis_iat_one" {
+  abort_statement {
+    message {
+      content_type = "PlainText"
+      content      = "Sorry, I could not understand. Please type 'Help'"
+    }
+  }
+
+  child_directed = false
+
+  clarification_prompt {
+    max_attempts = 2
+
+    message {
+      content_type = "PlainText"
+      content      = "Sorry, what can I help you with?"
+    }
+  }
+
+  description                 = "Bot to chat"
+  detect_sentiment            = false
+  idle_session_ttl_in_seconds = 600
+
+  //intent {
+    //intent_name    = "LATIS_greetingUser"
+    //intent_version = "1"
+    //intent_version = "$LATEST"
+    //intent_name    = aws_lex_intent.awsLATIS_greetingUser.name
+    //intent_version = aws_lex_intent.awsLATIS_greetingUser.version
+  //}
+intent {
+    intent_name    = aws_lex_intent.LATIS_greetingUser.name
+    intent_version = aws_lex_intent.LATIS_greetingUser.version
+  }
+
+  locale           = "en-US"
+  name             = "LATISgreetingUser"
+  process_behavior = "SAVE"
+  voice_id         = "Salli"
+}
