87a4c076a0a78205492a64b1b3ce3fc50e25464c
info <info@rivitech.co>
2021-08-30 19:34:34 +0530

    lex default added

--- bot.tf
+++ bot.tf
@@ -145,10 +145,10 @@ resource "aws_lex_bot" "vwt_corp_chatbot_latis_iat_one" {
     intent_name    = "LATIS_Customer_HELP"
     intent_version = "$LATEST"
   }
-  intent {
-    intent_name    = "LATIS_End_Greetings"
-    intent_version = "$LATEST"
-  }
+  # intent {
+  #   intent_name    = "LATIS_End_Greetings"
+  #   intent_version = "$LATEST"
+  # }
   intent {
     intent_name    = "LATIS_FA_Creation"
     intent_version = "$LATEST"
